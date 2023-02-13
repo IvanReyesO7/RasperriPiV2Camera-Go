9e59181027cdd5dedf50f5bf6167a15644cf8124
Ivan <ivanwilf@hotmail.com>
2023-02-13 12:44:43 +0000

    Added video controller

--- main.go
+++ main.go
@@ -1,35 +1,53 @@
 package main
 
 import (
-	"log"
+	controller "RasperriPiV2Camera-Go/controller"
 	"net/http"
 
-	"github.com/hybridgroup/mjpeg"
-	"gocv.io/x/gocv"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	webcam, err := gocv.VideoCaptureDevice(0)
-	if err != nil {
-		log.Fatal("Error getting video device: ", err)
-		return
-	}
-	defer webcam.Close()
-	stream := mjpeg.NewStream()
+	gin.SetMode(gin.ReleaseMode)
+	server := gin.Default()
 
-	go func() {
-		for {
-			img := gocv.NewMat()
-			defer img.Close()
-			webcam.Read(&img)
+	server.Static("/assets", "./assets")
+	server.LoadHTMLGlob("templates/index.html")
 
-			buf, _ := gocv.IMEncode(".jpg", img)
-			stream.UpdateJPEG(buf.GetBytes())
-		}
-	}()
-	http.Handle("/", stream)
-	err = http.ListenAndServe(":8080", nil)
-	if err != nil {
-		panic(err)
-	}
+	server.GET("/", func(c *gin.Context) {
+
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"status": http.StatusOK,
+		})
+	})
+
+	server.GET("/video", controller.Video)
+
+	server.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+
+// func main() {
+// 	webcam, err := gocv.VideoCaptureDevice(0)
+// 	if err != nil {
+// 		log.Fatal("Error getting video device: ", err)
+// 		return
+// 	}
+// 	defer webcam.Close()
+// 	stream := mjpeg.NewStream()
+
+// 	go func() {
+// 		for {
+// 			img := gocv.NewMat()
+// 			defer img.Close()
+// 			webcam.Read(&img)
+
+// 			buf, _ := gocv.IMEncode(".jpg", img)
+// 			stream.UpdateJPEG(buf.GetBytes())
+// 		}
+// 	}()
+// 	http.Handle("/", stream)
+// 	err = http.ListenAndServe(":8080", nil)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
