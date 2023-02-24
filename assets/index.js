b513bd8f1354f343b9fba334c879bdb32fd2b49c
Ivan <ivanwilf@hotmail.com>
2023-02-24 14:37:42 +0000

    Added functuinalirt on/off preview

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+let placeholder = "https://www.shoshinsha-design.com/wp-content/uploads/2020/05/noimage.png"
+
+$('#preview-btn').click(function(){
+    $(this).toggleClass('btn-primary btn-danger');
+    if ($(this).text() == "On"){
+       $(this).text("Off");
+       $('#video-frame').attr('src', '/video');
+    } else {
+       $(this).text("On");
+       $('#video-frame').attr('src', placeholder);
+    }
+})
